68cf1361ff6ba5e47cc2d6ab070ae0feaa9a46c7
shaharivka <57682267+shaharivka@users.noreply.github.com>
2021-07-18 14:54:40 +0300

    BigML

--- BigMl.js
+++ BigMl.js
@@ -1,20 +1,27 @@
 
 
-var bigml = require('bigml');
-var connection = bigml.BigML('shaharglikman00',
-                              '31ad2a093477b42b7417905e933564631112dbd8');
- var source = new bigml.Source(connection);
- var see='./data/iris.csv';
+const bigml = require('bigml');
+const username='shaharglikman00';
+const password='31ad2a093477b42b7417905e933564631112dbd8';
+//const project='project/60ef024de4279b249b002bcc';
+const connection = new bigml.BigML(username,password);
+connection.project='project/60ef024de4279b249b002bcc';
+connection.organization="http://www.ilo.org/global/lang--en/index.htm";
+const source = new bigml.Source(connection);
+const see='./data/iris.csv';
   source.create(see, function(error, sourceInfo) {
   if (!error && sourceInfo) {
-    var dataset = new bigml.Dataset();
+    var dataset = new bigml.Dataset(connection);
     dataset.create(sourceInfo, function(error, datasetInfo) {
       if (!error && datasetInfo) {
-        var model = new bigml.Model();
+        var model = new bigml.Model(connection);
         model.create(datasetInfo, function (error, modelInfo) {
           if (!error && modelInfo) {
-            var prediction = new bigml.Prediction();
-            prediction.create(modelInfo, {'petal length': 1})
+             var prediction = new bigml.Prediction(connection);
+             
+             setTimeout(() => { }, 5000);  //18/7 maybe a different way
+             prediction.create(modelInfo, {'petal length': 1})
+             
           }
         });
       }
